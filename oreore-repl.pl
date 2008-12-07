d674a7ea810baaf47d7e17b887fed4d0f20cc6af
Tokuhiro Matsuno <tokuhirom@skinny.local>
2008-12-07 23:58:37 +0900

    implemented

---
+++ oreore-repl.pl
@@ -0,0 +1,12 @@
+use strict;
+use warnings;
+use FindBin::libs;
+use OreOre::Tokuhirom::REPL;
+use File::Spec;
+use File::HomeDir;
+
+my $conffile = File::Spec->catfile(File::HomeDir->my_home());
+
+my $repl = OreOre::Tokuhirom::REPL->new({config => $conffile});
+$repl->run;
+
