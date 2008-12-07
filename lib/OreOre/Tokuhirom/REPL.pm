603cbdc8f68c9a9b07d765f68c90f0ed550ef14b
Tokuhiro Matsuno <tokuhirom@skinny.local>
2008-12-08 00:37:36 +0900

    added package name hack

--- lib/OreOre/Tokuhirom/REPL.pm
+++ lib/OreOre/Tokuhirom/REPL.pm
@@ -3,55 +3,35 @@ use strict;
 use warnings;
 use 5.00800;
 our $VERSION = '0.01';
-use Class::Component;
 use Term::ReadLine;
 use Term::ANSIColor;
-__PACKAGE__->load_components(qw(
-    Plaggerize
-));
+use Data::Dumper;
+use Class::Trigger;
+
+our $PACKAGE = 'main';
 
 sub run {
     my $rl = Term::ReadLine->new('OreOre::REPL');
-    while (defined (local $_ = $rl->readline('repl>'))) {
+    my $counter = 1;
+    while (defined (local $_ = $rl->readline("repl($PACKAGE)> "))) {
         next unless $_;
-        print eval $_;
-        print "\n";
+
+        eval {
+            my $code = eval "package $PACKAGE;sub { $_; BEGIN { \$PACKAGE = __PACKAGE__ }}; ";
+            die $@ if $@;
+
+            my $res = $code->();
+            print Dumper($res) if $res;
+            print "\n";
+
+            $rl->addhistory($_);
+        };
         if (my $e = $@) {
             print STDERR color 'red bold';
             print STDERR $e;
             print STDERR color 'reset';
-        } else {
-            $rl->addhistory($_);
         }
     }
 }
 
 1;
-__END__
-
-=encoding utf8
-
-=head1 NAME
-
-OreOre::Tokuhirom::REPL -
-
-=head1 SYNOPSIS
-
-  use OreOre::Tokuhirom::REPL;
-
-=head1 DESCRIPTION
-
-OreOre::Tokuhirom::REPL is
-
-=head1 AUTHOR
-
-Tokuhiro Matsuno E<lt>tokuhirom@gmail.comE<gt>
-
-=head1 SEE ALSO
-
-=head1 LICENSE
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
--- oreore-repl.pl
+++ oreore-repl.pl
@@ -3,11 +3,6 @@ use strict;
 use warnings;
 use FindBin::libs;
 use OreOre::Tokuhirom::REPL;
-use File::Spec;
-use File::HomeDir;
 
-my $conffile = File::Spec->catfile(File::HomeDir->my_home());
-
-my $repl = OreOre::Tokuhirom::REPL->new({config => $conffile});
-$repl->run;
+OreOre::Tokuhirom::REPL->run();
 
