116ab490559d634c2c05aae4df2cb441a0b9e560
Tokuhiro Matsuno <tokuhirom@gp.ath.cx>
2008-12-10 11:37:50 +0900

    added perldoc support

--- lib/OreOre/REPL.pm
+++ lib/OreOre/REPL.pm
@@ -45,7 +45,11 @@ sub show_banner {
 sub run_once {
     my ($class, $c, $src) = @_;
 
-    if (my ($cmd, $args) = ($src =~ /^:([a-z]+)(?:\s+(\S+))?$/)) {
+    if (my ($args) = ($src =~ /^\?\s*(.+)$/)) {
+        # help
+        $c->run_help($args);
+    } elsif (my ($cmd, $args) = ($src =~ /^:([a-z]+)(?:\s+(\S+))?$/)) {
+        # run builtin command
         my $meth = "cmd_$cmd";
         if ($c->can($meth)) {
             $c->$meth($args);
@@ -105,7 +109,37 @@ sub cmd_help {
 *loading plugins
     :load Memory
     :load Yosh
+*more help
+    ? Digest::MD5
 ...
+}
+
+my %perlfuncs = 
+    map { $_ => 1 }
+qw(
+  abs accept alarm atan bind binmode bless break caller chdir chmod chomp chop chown chr chroot close closedir connect
+  continue cos crypt dbmclose dbmopen defined delete die do dump each endgrent endhostent endnetent endprotoent endpwent
+  endservent eof eval exec exists exit exp fcntl fileno flags flock fork format formline getc getgrent getgrgid getgrnam
+  gethostbyaddr gethostbyname gethostent getlogin getnetbyaddr getnetbyname getnetent getpeername getpgrp getppid
+  getpriority getprotobyname getprotobynumber getprotoent getpwent getpwnam getpwuid getservbyname getservbyport getservent
+  getsockname getsockopt glob gmtime goto grep hex import index int ioctl join keys kill last lc lcfirst length link
+  listen local localtime lock log lstat m map mkdir msgctl msgget msgrcv msgsnd my next no oct open opendir ord order
+  our pack package pipe pop pos precision print printf prototype push q qq qr quotemeta qw qx rand read readdir readline
+  readlink readpipe recv redo ref rename require reset return reverse rewinddir rindex rmdir s say scalar seek seekdir select
+  semctl semget semop send setgrent sethostent setnetent setpgrp setpriority setprotoent setpwent setservent setsockopt shift
+  shmctl shmget shmread shmwrite shutdown sin size sleep socket socketpair sort splice split sprintf sqrt srand stat state
+  study sub substr symlink syscall sysopen sysread sysseek system syswrite tell telldir tie tied time times tr truncate uc
+  ucfirst umask undef unlink unpack unshift untie use utime values vec vector wait waitpid wantarray warn write y
+);
+
+sub run_help {
+    my ($c, $args) = @_;
+    $args =~ s/^\s+//;
+    $args =~ s/\s+$//;
+    if ($args =~ /^[a-z]+$/ && $perlfuncs{$args}) {
+        $args = "-f $args";
+    }
+    system "perldoc $args";
 }
 
 1;
